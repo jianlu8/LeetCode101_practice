a63902ed00957bdf184bf59e96def3f39580535b
Jianlu8 <limitlujian@163.com>
2022-03-16 19:22:49 +0800

    update some sort code

--- 540.cc
+++ 540.cc
@@ -12,7 +12,7 @@ public:
         int low = 0, high = nums.size() - 1;
         while(low < high) {
             int mid = (high - low) / 2 + low;
-            if (nums[mid] == nums[mid-1]) {
+            if (nums[mid] == nums[mid^1]) {
                 low = mid + 1;
             } else {
                 high = mid;
