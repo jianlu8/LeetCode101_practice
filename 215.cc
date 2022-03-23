2c397798e17a1e210bfaf88be887dcd852baa3a8
jianlu8 <limitlujian@163.com>
2022-03-23 20:25:34 +0800

    add 215.cc

--- 215.cc
+++ 215.cc
@@ -9,43 +9,18 @@ using namespace std;
 class Solution {
    public:
     int findKthLargest(vector<int>& nums, int k) {
-        int l = 0, r = nums.size() - 1, target = nums.size() - k;
-        while (l < r) {
-            int mid = quickSelection(nums, l, r);
-            if (mid == target) {
-                return nums[mid];
-            }
-            if (mid < target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i; j > 0 && nums[j] < nums[j - 1]; --j) {
+                swap(nums[j], nums[j - 1]);
             }
         }
-        return nums[l];
-    }
-    // 辅函数 - 快速选择
-    int quickSelection(vector<int>& nums, int l, int r) {
-        int i = l + 1, j = r;
-        while (true) {
-            while (i < r && nums[i] <= nums[l]) {
-                ++i;
-            }
-            while (l < j && nums[j] >= nums[l]) {
-                --j;
-            }
-            if (i >= j) {
-                break;
-            }
-            swap(nums[i], nums[j]);
-        }
-        swap(nums[l], nums[j]);
-        return j;
+        return nums[nums.size()-k];
     }
 };
 
 int main(int argc, char const* argv[]) {
     vector<int> nums = {5, 7, 7, 8, 8, 10, 10};
-    int k = 1;
+    int k = 2;
 
     Solution sol;
 
