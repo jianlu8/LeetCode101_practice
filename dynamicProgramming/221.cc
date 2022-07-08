eddb12026b35fcd1ef42ca77a199ee59343cde18
jianlu8 <limitlujian@163.com>
2022-07-08 10:10:07 +0800

    add 221.cc

--- dynamicProgramming/221.cc
+++ dynamicProgramming/221.cc
@@ -9,48 +9,34 @@ using namespace std;
 
 class Solution {
  public:
-  vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-    if (matrix.empty()) return {};
-    int n = matrix.size(), m = matrix[0].size();
-    vector<vector<int>> dp(n, vector<int>(m, INT_MAX - 1));
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < m; ++j) {
-        if (matrix[i][j] == 0) {
-          dp[i][j] = 0;
-        } else {
-          if (j > 0) {
-            dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
-          }
-          if (i > 0) {
-            dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
-          }
+  int maximalSquare(vector<vector<char>>& matrix) {
+    if (matrix.empty() || matrix[0].empty()) {
+      return 0;
+    }
+    int m = matrix.size(), n = matrix[0].size(), max_side = 0;
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+    for (int i = 1; i <= m; ++i) {
+      for (int j = 1; j <= n; ++j) {
+        if (matrix[i - 1][j - 1] == '1') {
+          dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
         }
+        max_side = max(max_side, dp[i][j]);
       }
     }
-    for (int i = n - 1; i >= 0; --i) {
-      for (int j = m - 1; j >= 0; --j) {
-        if (matrix[i][j] != 0) {
-          if (j < m - 1) {
-            dp[i][j] = min(dp[i][j], dp[i][j + 1] + 1);
-          }
-          if (i < n - 1) {
-            dp[i][j] = min(dp[i][j], dp[i + 1][j] + 1);
-          }
-        }
-      }
-    }
-    return dp;
+    return max_side * max_side;
   }
 };
 
+
+
 int main(int argc, char const* argv[]) {
-  vector<vector<int>> n = {{0, 0, 0},   //  1
-                           {0, 1, 0},   //
-                           {1, 1, 1}};  //
-  //                 0,0,1,2,0,0,1
+  vector<vector<char>> n ={{'1','0','1','1'},
+                           {'1','1','1','1'},
+                           {'1','1','1','1'},
+                           {'1','1','1','1'}};
   Solution sol;
 
-  vector<vector<int>> out = sol.updateMatrix(n);
+  int out = sol.maximalSquare(n);
 
-  cout << out[2][1] << std::endl;
+  cout << out << endl;
 }
